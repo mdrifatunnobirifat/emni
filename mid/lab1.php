297476b84da49a3a307debc15fca74a084031352
mdrifatunnobirifat <mdrifatunnabi@gmail.com>
2025-07-25 17:55:56 +0600

    Add files via upload

---
+++ mid/lab1.php
@@ -0,0 +1,54 @@
+<!doctype html>
+<html>
+    <title> registrstion form </title>
+<body>
+<center>
+    <h1 style="color:blue;">AIUB</h1>
+    <h2 style="color:blue;">Registraion From</h2>
+</center>
+<table>
+<p><b> Complete the Registraion</b></p>
+<tr>
+    <td>Full employee  Name: </td><td> <input type="text"></td></tr>
+<tr>
+    <td>Email: </td><td> <input type="text"></td></tr>
+<tr>
+    <td>Password:</td><td><input type="text"></td></tr>
+<tr>
+    <td>Gender:</td><td><input type="radio" name="des">Male
+                        <input type="radio" name="des">Other</td>
+</tr>
+<tr>
+    <td>Language Known:</td><td><input type="checkbox" >English
+                        <input type="checkbox">Bangla
+                        <input type="checkbox">Hindi
+                    </td>
+</tr>
+<tr>
+    <td>Country:</td>
+    <td> <select name="Country:">
+           <option value="1">--select--</option>
+           <option value="2">Bangladesh</option>
+           <option value="3">America</option>
+       </select> </td>
+</tr>
+<tr>
+    <td>Date of Birth</td>
+    <td><input type="date"></td>
+</tr>
+<tr>
+    <td>Upload Photo:</td>
+    <td><input type="file"></td>
+</tr>
+<tr>
+    <td>Comments</td>
+    <td><input type="textarea"></td>
+</tr>
+
+<tr>
+    <td align="right"><input type="submit" name="Register"></td>
+</tr>
+
+</table>
+</body>
+</html>
